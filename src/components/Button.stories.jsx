8f70fc140ee5afa0a1bbe5d4a5be423769309c69
hoshico <melancholinista.yasu@gmail.com>
2022-04-06 23:54:43 +0900

    add Button.stories.jsx

---
+++ src/components/Button.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Button } from './Button';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Components/Button',
+  component: Button,
+};
+
+export const ButtonComponents = () => <Button label='ボタン' onClick={() => {}} />;
